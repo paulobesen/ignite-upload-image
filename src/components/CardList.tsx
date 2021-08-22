595052eaf8982d580c122fd1aaa9d5ef6f6eac17
Paulo Roberto Besen da Ventura <paulobesen@gmail.com>
2021-08-22 11:47:46 -0300

    challenge complete

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,32 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  // MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  // SELECTED IMAGE URL STATE
+  const [urlImg, setUrlImg] = useState('');
 
-  // TODO SELECTED IMAGE URL STATE
-
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  // FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string): void {
+    setUrlImg(url);
+    onOpen();
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing={40}>
+        {cards.map(card => (
+          <Card
+            key={card.id}
+            data={card}
+            viewImage={() => handleViewImage(card.url)}
+          />
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      {urlImg && (
+        <ModalViewImage isOpen={isOpen} imgUrl={urlImg} onClose={onClose} />
+      )}
     </>
   );
 }
--- src/components/Form/FormAddImage.tsx
+++ src/components/Form/FormAddImage.tsx
@@ -7,6 +7,12 @@ import { api } from '../../services/api';
 import { FileInput } from '../Input/FileInput';
 import { TextInput } from '../Input/TextInput';
 
+interface FormImageProps {
+  image: FileList;
+  title: string;
+  description: string;
+  url: string;
+}
 interface FormAddImageProps {
   closeModal: () => void;
 }
@@ -15,46 +21,79 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
   const [imageUrl, setImageUrl] = useState('');
   const [localImageUrl, setLocalImageUrl] = useState('');
   const toast = useToast();
+  const regex = new RegExp(/\.(jpeg|jpg|png|gif)$/i);
 
   const formValidations = {
     image: {
-      // TODO REQUIRED, LESS THAN 10 MB AND ACCEPTED FORMATS VALIDATIONS
+      // REQUIRED, LESS THAN 10 MB AND ACCEPTED FORMATS VALIDATIONS
+      required: 'Arquivo obrigatório',
+      validate: {
+        lessThan10MB: file =>
+          file[0].size < 10000000 || 'O arquivo deve ser menor que 10MB',
+        acceptedFormats: file =>
+          regex.test(file[0].name) ||
+          'Somente são aceitos arquivos PNG, JPEG e GIF',
+      },
     },
     title: {
-      // TODO REQUIRED, MIN AND MAX LENGTH VALIDATIONS
+      // REQUIRED, MIN AND MAX LENGTH VALIDATIONS
+      required: 'Título obrigatório',
+      minLength: { value: 2, message: 'Mínimo de 2 caracteres' },
+      maxLength: { value: 20, message: 'Máximo de 20 caracteres' },
     },
     description: {
-      // TODO REQUIRED, MAX LENGTH VALIDATIONS
+      // REQUIRED, MAX LENGTH VALIDATIONS
+      required: 'Descrição obrigatória',
+      maxLength: { value: 65, message: 'Máximo de 65 caracteres' },
     },
   };
 
   const queryClient = useQueryClient();
   const mutation = useMutation(
-    // TODO MUTATION API POST REQUEST,
+    (payload: FormImageProps) => api.post('/api/images', payload),
     {
-      // TODO ONSUCCESS MUTATION
+      onSuccess: () => {
+        queryClient.invalidateQueries('images');
+      },
     }
   );
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState,
-    setError,
-    trigger,
-  } = useForm();
+  const { register, handleSubmit, reset, formState, setError, trigger } =
+    useForm();
   const { errors } = formState;
 
-  const onSubmit = async (data: Record<string, unknown>): Promise<void> => {
+  const onSubmit = async (data: FormImageProps): Promise<void> => {
     try {
-      // TODO SHOW ERROR TOAST IF IMAGE URL DOES NOT EXISTS
-      // TODO EXECUTE ASYNC MUTATION
-      // TODO SHOW SUCCESS TOAST
+      // SHOW ERROR TOAST IF IMAGE URL DOES NOT EXISTS
+      if (!imageUrl) {
+        toast({
+          status: 'error',
+          title: 'Imagem não enviada',
+          isClosable: true,
+        });
+        return;
+      }
+
+      // EXECUTE ASYNC MUTATION
+      mutation.mutate(data);
+
+      // SHOW SUCCESS TOAST
+      toast({
+        title: 'Imagem enviada com sucesso',
+        status: 'success',
+      });
     } catch {
-      // TODO SHOW ERROR TOAST IF SUBMIT FAILED
+      // SHOW ERROR TOAST IF SUBMIT FAILED
+      toast({
+        title: 'Erro ao enviar a imagem',
+        status: 'error',
+        isClosable: true,
+      });
     } finally {
-      // TODO CLEAN FORM, STATES AND CLOSE MODAL
+      // CLEAN FORM, STATES AND CLOSE MODAL
+      reset();
+      closeModal();
+      setImageUrl('');
     }
   };
 
@@ -67,20 +106,26 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
           setLocalImageUrl={setLocalImageUrl}
           setError={setError}
           trigger={trigger}
-          // TODO SEND IMAGE ERRORS
-          // TODO REGISTER IMAGE INPUT WITH VALIDATIONS
+          // SEND IMAGE ERRORS
+          error={errors.image}
+          // REGISTER IMAGE INPUT WITH VALIDATIONS
+          {...register('image', formValidations.image)}
         />
 
         <TextInput
           placeholder="Título da imagem..."
-          // TODO SEND TITLE ERRORS
-          // TODO REGISTER TITLE INPUT WITH VALIDATIONS
+          // SEND TITLE ERRORS
+          error={errors.title}
+          // REGISTER TITLE INPUT WITH VALIDATIONS
+          {...register('title', formValidations.title)}
         />
 
         <TextInput
           placeholder="Descrição da imagem..."
-          // TODO SEND DESCRIPTION ERRORS
-          // TODO REGISTER DESCRIPTION INPUT WITH VALIDATIONS
+          // SEND DESCRIPTION ERRORS
+          error={errors.description}
+          // REGISTER DESCRIPTION INPUT WITH VALIDATIONS
+          {...register('description', formValidations.description)}
         />
       </Stack>
 
